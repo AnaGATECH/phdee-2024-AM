f7eb760d77286707af33c3fada62bb30e37dc808
anamazmishvili <157053121+AnaGATECH@users.noreply.github.com>
2024-02-09 22:41:31 -0500

    Create HW4_Py_code.py

---
+++ homework 4/code/HW4_Py_code.py
@@ -0,0 +1,41 @@
+# HW 4 code -- Ana Mazmishvili
+
+from IPython import get_ipython
+get_ipython().magic('reset -sf')
+
+# Import packages 
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import statsmodels.api as sm
+from scipy import stats
+import csv
+from scipy.optimize import minimize
+import statsmodels.api as sm
+from sklearn.linear_model import LinearRegression as lr
+
+
+# Set working directories and seed
+
+#If working from home:
+
+datapath = r'C:\Users\Owner\Dropbox\phdee-2024-AM\homework 4\data'
+outputpath = r'C:\Users\Owner\Dropbox\phdee-2024-AM\homework 4\output'
+
+# If working on campus: 
+
+#datapath = r'C:\Users\amazmishvili3\Dropbox\phdee-2024-AM\homework 4\data'
+#outputpath = r'C:\Users\amazmishvili3\Dropbox\phdee-2024-AM\homework 4\output'
+
+
+np.random.seed(4)
+
+# Load dataset in the system 
+
+data=pd.read_csv(datapath +'/fishbycatch.csv')
+
+
+### Question 1.e ###
+# -------------------------------------------------------------------------- #
