e321c4209a9d0d0324d3f50011b574a817d564bf
AnaGATECH <157053121+AnaGATECH@users.noreply.github.com>
2024-02-19 16:22:18 -0500

    Problem 4

--- homework 5/code/Python_code_HW5.py
+++ homework 5/code/Python_code_HW5.py
@@ -16,6 +16,8 @@ import statsmodels.api as sm
 # After that this code was executed. 
 from stargazer.stargazer import Stargazer as stargazer
 from stargazer.stargazer import LineLocation
+from linearmodels import IVGMM
+
 
 
 
@@ -23,15 +25,17 @@ from stargazer.stargazer import LineLocation
 
 #If working from home:
 
-datapath = r'C:\Users\Owner\Dropbox\phdee-2024-AM\homework 5\data'
-outputpath = r'C:\Users\Owner\Dropbox\phdee-2024-AM\homework 5\output'
-figure = r'C:\Users\Owner\Dropbox\phdee-2024-AM\homework 5\output\figure'
-table = r'C:\Users\Owner\Dropbox\phdee-2024-AM\homework 5\output\table'
+#datapath = r'C:\Users\Owner\Dropbox\phdee-2024-AM\homework 5\data'
+#outputpath = r'C:\Users\Owner\Dropbox\phdee-2024-AM\homework 5\output'
+#figure = r'C:\Users\Owner\Dropbox\phdee-2024-AM\homework 5\output\figure'
+#table = r'C:\Users\Owner\Dropbox\phdee-2024-AM\homework 5\output\table'
 
 # If working on campus: 
 
-#datapath = r'C:\Users\amazmishvili3\Dropbox\phdee-2024-AM\homework 5\data'
-#outputpath = r'C:\Users\amazmishvili3\Dropbox\phdee-2024-AM\homework 5\output'
+datapath = r'C:\Users\amazmishvili3\Dropbox\phdee-2024-AM\homework 5\data'
+outputpath = r'C:\Users\amazmishvili3\Dropbox\phdee-2024-AM\homework 5\output'
+figure = r'C:\Users\amazmishvili3\Dropbox\phdee-2024-AM\homework 5\output\figure'
+table = r'C:\Users\amazmishvili3\Dropbox\phdee-2024-AM\homework 5\output\table'
 
 os.chdir(datapath)
 
@@ -66,18 +70,15 @@ xvar3a_1 = sm.add_constant(data[['weight','car']])
 ols3a_1 = sm.OLS(yvar3a,xvar3a_1.astype(float)).fit()
 fit_mpg3a = ols3a_1.fittedvalues.to_frame()
 
-## I need here to access the F value of instrument
-
-#inst_f_stat = ols3a_1.f_test([0, 1]).fvalue
-
 ## This gives me F value for the whole regression.
-#inst_f_stat = ols3a_1.fvalue
-#print (inst_f_stat)
+fstat_a = ols3a_1.fvalue
+fstat_a = round(fstat_a, 2)
+print (fstat_a)
 
 
 
 ## The Second Stage
-xvar3a_2 = pd.concat([data[['weight','car']], fit_mpg3a], axis=1)
+xvar3a_2 = pd.concat([data[['car']], fit_mpg3a], axis=1)
 xvar3a_2.rename(columns={0: 'fit_mpg'}, inplace=True)
 
 ols3a_2 = sm.OLS(yvar1,sm.add_constant(xvar3a_2, prepend = False).astype(float)).fit()
@@ -100,9 +101,14 @@ xvar3b_1 = sm.add_constant(data[['weight2','car']])
 ols3b_1 = sm.OLS(yvar3a,xvar3b_1.astype(float)).fit()
 fit_mpg3b = ols3b_1.fittedvalues.to_frame()
 
+## This gives me F value for the whole regression.
+fstat_b = ols3b_1.fvalue
+fstat_b = round(fstat_b, 2)
+print (fstat_b)
+
 
 ## The Second Stage
-xvar3b_2 = pd.concat([data[['weight2','car']],fit_mpg3b], axis=1)
+xvar3b_2 = pd.concat([data[['car']],fit_mpg3b], axis=1)
 xvar3b_2.rename(columns={0: 'fit_mpg'}, inplace=True)
 
 ols3b_2 = sm.OLS(yvar1,sm.add_constant(xvar3b_2, prepend = False).astype(float)).fit()
@@ -118,8 +124,13 @@ xvar3c_1 = sm.add_constant(data[['height','car']])
 ols3c_1 = sm.OLS(yvar3c,xvar3c_1.astype(float)).fit()
 fit_mpg3c = ols3c_1.fittedvalues.to_frame()
 
+## This gives me F value for the whole regression.
+fstat_c = ols3c_1.fvalue
+fstat_c = round(fstat_c, 2)
+print (fstat_c)
+
 ## The Second Stage
-xvar3c_2 = pd.concat([data[['height','car']],fit_mpg3c], axis=1)
+xvar3c_2 = pd.concat([data[['car']],fit_mpg3c], axis=1)
 xvar3c_2.rename(columns={0: 'fit_mpg'}, inplace=True)
 
 ols3c_2 = sm.OLS(yvar1,sm.add_constant(xvar3c_2, prepend = False).astype(float)).fit()
@@ -133,10 +144,9 @@ print(output)
 
 
 
-output.covariate_order(['fit_mpg','weight','weight2','height','car'])
-output.rename_covariates({'fit_mpg':'MPG','weight':'Weight','weight2':'Squared Weight','height':'Height','car':'Car'})
-# I can add here manually F statostics
-#output.add_line('Month indicators',['Y','Y','Y'], LineLocation.FOOTER_TOP)
+output.covariate_order(['fit_mpg','car'])
+output.rename_covariates({'fit_mpg':'MPG','car':'Car'})
+output.add_line('F-statistics from the 1st Stage',[fstat_a,fstat_b,fstat_c], LineLocation.FOOTER_TOP)
 output.significant_digits(2)
 output.show_degrees_of_freedom(False)
 
@@ -155,5 +165,22 @@ endog = data['mpg']
 exog = sm.add_constant(data['car'])
 instrument = data['weight']
 
-# ivgmm = sm.IVGMM(yvar1, endog, exog, instrument, prepend = False).fit()
+ivgmm=IVGMM(yvar1, exog,  endog, instrument).fit()
+
+# Output table with Stargazer package  ----------------------------------------
+
+coef_gmm = ivgmm.params
+se_gmm = ivgmm.std_errors
+
+GMM_table=pd.DataFrame(
+    {'IVGMM': ["{:0.2f}".format(coef_gmm['mpg']), "({:0.2f})".format(se_gmm['mpg']), 
+             "{:0.2f}".format(coef_gmm['car']), "({:0.2f})".format(se_gmm['car'])]},
+     index=['Miles per gallon', ' ',
+            '=1 if the vehicle is sedan', ' '])
+os.chdir(table)
+GMM_table.to_latex('IVGMM.tex', column_format='lcc', float_format="%.2f", escape=False)
+
+
+
+
 
